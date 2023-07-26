22b57897bf0c5766c6ec0cb6071416022d78ddbf
Anthony Goeckner <anthony.goeckner@northwestern.edu>
2023-07-26 01:03:26 -0500

    Added static map to odom publisher.

--- src/grex/launch/simulator/flatland/agent.launch.py
+++ src/grex/launch/simulator/flatland/agent.launch.py
@@ -1,6 +1,7 @@
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, ExecuteProcess
-from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, FindExecutable
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, FindExecutable, TextSubstitution
+from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
@@ -21,6 +22,17 @@ def generate_launch_description():
         ),
         DeclareLaunchArgument(
             'initial_pos_theta', default_value='0.0'
+        ),
+
+        DeclareLaunchArgument(
+            '_odom_frame_concat', default_value=[LaunchConfiguration('namespace'), '/odom']
+        ),
+
+        Node(
+            name='tf_map_to_odom',
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            arguments=['0', '0', '0', '0', '0', '0', 'map', LaunchConfiguration('_odom_frame_concat')],
         ),
 
         ExecuteProcess(
