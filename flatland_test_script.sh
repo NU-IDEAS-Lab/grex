c077be2c13f1a09c2c230b17fffd879f1a6c45d4
gyaanantia <>
2023-08-04 15:19:43 -0500

    clean up test scripts

--- flatland_test_script.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-ros2 launch grex simulation.launch.py simulator_launch_file:=/home/gyaan/mas_simulation/src/grex/launch/simulator/flatland/simulator.launch.yaml agent_count:=4 simulator_agent_integration_launch_file:=/home/gyaan/mas_simulation/src/grex/launch/simulator/flatland/agent.launch.py use_rviz:=False 
